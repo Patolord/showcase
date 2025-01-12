21bc22cafb4c021595c320c3c23fe2363b44e878
Patolord <rodrigo_luizp@hotmail.com>
2025-01-12 19:37:48 -0300

    Refactor Footer component for improved layout and styling; enhance email link accessibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,17 +3,18 @@ import { MailIcon } from "lucide-react";
 export default function Footer() {
   return (
     <footer className="bg-background border-t mt-auto sticky bottom-0">
-      <div className="container mx-auto px-4 py-4 text-center flex items-center justify-between gap-10 ">
-        <p>
+      <div className="container mx-auto px-4 py-4 text-center flex flex-col sm:flex-row items-center justify-between gap-2 sm:gap-10">
+        <p className="text-sm sm:text-base">
           &copy; {new Date().getFullYear()} Rodrigo Pellegrini. All rights
           reserved.
         </p>
-        <p className="">
+        <p className="text-sm sm:text-base">
           <a
             href="mailto:rodrigo_luizp@outlook.com"
             className="text-primary hover:underline flex items-center gap-2"
           >
-            <MailIcon /> rodrigo_luizp@outlook.com
+            <MailIcon className="h-4 w-4 sm:h-5 sm:w-5" />
+            rodrigo_luizp@outlook.com
           </a>
         </p>
       </div>
